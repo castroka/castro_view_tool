b55b620f03a668003eb31a0e7bc0bc4ac07ce895
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:03:54 +0000

    Castro tool initial commit

---
+++ lib/castro_view_tool.rb
@@ -0,0 +1,6 @@
+require "castro_view_tool/version"
+require "castro_view_tool/renderer"
+
+module CastroViewTool
+  # Your code goes here...
+end
---
+++ lib/castro_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CastroViewTool
+  VERSION = "0.1.0"
+end
